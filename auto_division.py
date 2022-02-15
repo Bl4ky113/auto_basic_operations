0aa1353e016b6ddd99a3ae46722d8b5a36413618
Bl4ky113 <martinstevenhernandezortiz@gmail.com>
2022-02-14 19:41:47 -0500

    Base auto_division

---
+++ auto_division.py
@@ -0,0 +1,11 @@
+
+from random import randint 
+
+def division_process (num_1, num_2):
+    pass
+
+if __name__ == "__main__":
+    num_div_1 = randint(10 ** 8, 10 ** 8)
+    num_div_2 = randint(1, 10)
+
+    result = division_process(num_div_1, num_div_2)
