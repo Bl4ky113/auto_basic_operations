77483ebbb9f31d5dbfe95adf48825dc15061cdf8
Bl4ky113 <martinstevenhernandezortiz@gmail.com>
2022-02-15 20:22:29 -0500

    Added division process

--- auto_division.py
+++ auto_division.py
@@ -2,29 +2,37 @@
 from random import randint
 
 def update_log (log_arr, msg=""):
-    log_arr["log"] = msg
+    log_arr["log"].append(msg)
 
 def division_process (num_1="", num_2="", max_decimals=4):
     results_obj = {}
     results_obj["log"] = []
     results_obj["results"] = []
-    arr_num = list(num_1)
-    division_remaining = arr_num.copy()
-    divisor = num_2
 
-    for i in range(len(arr_num)):
-        div_scope = division_remaining[i: i + len(divisor)]
-        result = int("".join(div_scope)) / int(divisor)
-        residue = int("".join(div_scope)) % int(divisor)
+    division_remaining = list(num_1).copy()
+    for i in range(max_decimals):
+        division_remaining.append("0")
+    divisor = int(num_2)
+    len_divisor = len(num_2)
+    residue = "0"
+
+    for i in range(len(division_remaining)):
+        div_scope = division_remaining[i: i + len_divisor]
+        div_scope.insert(0, residue)
+        result = int("".join(div_scope)) // divisor
 
         update_log(results_obj, f"Division: {div_scope} / {divisor}")
+        update_log(results_obj, f"""{"Can't do the " if result == 0 else ""}Division: {result}""")
 
-        if result >= 1:
-            results_obj["results"].append(str(result))
-            update_log(results_obj, f"Divided: {result}")
+        if result != 0:
+            residue = str(int("".join(div_scope)) % divisor)
+            update_log(results_obj, f"Residue: {residue}")
         else:
-            results_obj["results"].append("0")
-            update_log(results_obj, f"Can't divide: 0")
+            residue = "".join(div_scope)
+
+        results_obj["results"].append(result)
+
+    results_obj["total"] = round(int("".join(num_1)) / divisor, max_decimals)
 
     return results_obj
 
@@ -40,4 +48,8 @@ if __name__ == "__main__":
     print(f"{num_div_1} / {num_div_2}")
 
     for key, val in results.items():
-        print(key, val)
+        if key == "log":
+            for log in val:
+                print(log)
+        else:
+            print(key, val)
